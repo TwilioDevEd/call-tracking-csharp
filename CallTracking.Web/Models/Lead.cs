c7b1193d6f7af5f030b1e6dc26d658afbfc5c6f6
Agustin Camino <agustin.camino@gmail.com>
2015-09-14 11:50:26 -0500

    add LeadSource and Lead models

---
+++ CallTracking.Web/Models/Lead.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CallTracking.Web.Models
+{
+    public class Lead
+    {
+        public int Id { get; set; }
+        public string PhoneNumber { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int LeadSourceId { get; set; }
+        public virtual LeadSource LeadSource { get; set; }
+    }
+}
---
+++ CallTracking.Web/Models/LeadSource.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CallTracking.Web.Models
+{
+    public class LeadSource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string IncomingNumberNational { get; set; }
+        public string IncomingNumberInternational { get; set; }
+        public string ForwardingNumber { get; set; }
+        public virtual IList<Lead> Leads { get; set; }
+    }
+}
