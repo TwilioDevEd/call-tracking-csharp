01ad68cbbef7b09a7bff66df3102ec4d4226644c
Agustin Camino <agustin.camino@gmail.com>
2015-09-14 11:57:14 -0500

    add context type

---
+++ CallTracking.Web/Models/CallTrackingContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace CallTracking.Web.Models
+{
+    public class CallTrackingContext : DbContext
+    {
+        public CallTrackingContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<LeadSource> LeadSources { get; set; }
+        public DbSet<Lead> Leads { get; set; }
+    }
+}
